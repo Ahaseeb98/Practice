ca5f4b8740f7ac0f2e25e6c23475a1f16e378814
Abdul Haseeb <ahaseeb12251998@gmail.com>
2018-02-23 23:36:02 +0500

    Practice

--- demo.js
+++ demo.js
@@ -1,1 +1,306 @@
-alert("Hello World");
+//                                      1. Declare a variable called username.
+//var username;
+//                        2. Declare a variable called myName & assign to it a string that represents your Full Name.
+//var myName = "Abdul Haseeb";
+//                                      a. declare a JS variable, titled message
+//var message;
+//                                      b. assign “Hello World” to the variable message
+//message = "hello World";
+//                                      c. display the message in alert box
+//alert(message);
+
+//                          3.Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+// var name = "Abdul Haseeb";
+// var age = "19 Years Old";
+// var Course = "SSUET";
+// var cName = "Mobile and Cloud Computing-Module A";
+// alert(name);
+// alert(age);
+// alert(Course);
+// alert(cName);
+
+//                         5. Write a script to display the following alert using one JS variable:
+
+// alert(" PIZZA \n PIZZ \n PIZ \n PI \n P ");
+
+//                                          6. Fortune Teller:
+
+// var x = "You will be a Software Developer Some Day Maybe";
+// alert(x);
+
+
+//              7. Declare a variable called email & assign to it a string that represents your Email Address (e.g. abc@xyz.com).
+
+// var email = "My Email is ABC@yahoo.com. Please Subscribe"
+// alert(email);
+
+//                     8. Declare a variable called book & give it the value “A smarter way to learn JavaScript”.
+
+// var book = "A Smarter way to learn Javascript"
+// alert(book);
+
+//                      9. Use the variables, declared in exercise 4 & show the details giving meaning to data.
+
+// var name = "My name is Abdul Haseeb";
+// var age = "I am 19 Years Old";
+// var Course = "I Study at SSUET";
+// var cName = "I take the classes of Mobile and Cloud Computing-Module A";
+// alert(name);
+// alert(age);
+// alert(Course);
+// alert(cName); 
+
+// 10. Write a script to display this in browser through JS
+
+// var y = "Yay i can display Html through Javascript";
+// docoment.write(y);
+
+
+
+//                                      variable for NUMBERS
+
+
+
+//                     1. Declare a variable called age & assign to it your age.
+// var age = "I am 19 years Old."
+// alert(age);
+
+//                     2. For example: “You have visited this site N times”
+// var visit = "Tou have visited this site 101 times"
+// alert(visit);
+
+//          3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
+// var birthYear = 1998;
+// var msg = "My birth Year is "+birthYear+"\n. Data type of my birthYear is Number";
+// document.write(msg);
+
+
+// 4. Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// var z = “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”;
+// document.write(z);
+
+
+
+//                                  VARIABLE NAMES : LEGAL & ILLEGAL
+
+
+
+//                         1. Declare 3 variables in one statement.
+
+// var x = 1;
+// var y = 2;
+// var z = 3;
+
+
+//                          2. Declare 5 legal & 5 illegal variable names.
+
+// var x = 1;
+// var y = 2;
+// var z = 3;
+// var a = 4;
+// var b = 5;
+
+// illigal variables name
+
+// var 5abc = 2;
+// var @ue = 3;
+// var %f = 5;
+// var *kl = 6;
+// var -as = 3;
+
+//                              3. Display this in your browser
+
+// var a = "a. A heading stating Rules for naming JS variables";
+// var b =" b. Variable names can only contain ______, ______,______ and ______.\nFor example $my_1stVariable\n";
+// var c = "c. Variables must begin with a ______, ______ or_____. For example $name, _name or name\n";
+// var d = "d. Variable names are case _________\n";
+// var e = "e. Variable names should not be JS _________";
+// document.write(a);
+// document.write(b);
+// document.write(c);
+// document.write(d);
+// document.write(e);
+
+
+
+//                                           MATH EXPRESSIONS
+
+
+//                  1. Write a program that take two numbers & add them in a new variable.
+
+// var a = 1;
+// var b = 2;
+// var c = a + b;
+// var d = "Sum of "+a+" and "+b+" is "+c+" ";
+// alert(d); 
+
+//                  2. Repeat task1 for subtraction, multiplication,  division & modulus.  
+
+
+// substraction
+
+// var a = 1;
+// var b = 2;
+// var c = a - b;
+// var d = "Subtraction of "+a+" and "+b+" is "+c+" ";
+// alert(d); 
+
+// multiplication
+
+// var a = 1;
+// var b = 2;
+// var c = a * b;
+// var d = " multiplication of "+a+" and "+b+" is "+c+" ";
+// alert(d); 
+
+//  division
+
+// var a = 1;
+// var b = 2;
+// var c = a / b;
+// var d = " division of "+a+" and "+b+" is "+c+" ";
+// alert(d); 
+
+//  Modulus
+
+// var a = 1;
+// var b = 2;
+// var c = a / b;
+// var d = " Modulus of "+a+" and "+b+" is "+c+" ";
+// alert(d); 
+
+
+//                                     3. Do the following using JS Mathematic Expressions
+// //      a. Declare a variable.
+// var a;
+// //      b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+// a = " Value after variable declaration is: ";
+// alert(a);
+// //      c. Initialize the variable with some number.
+// var val1 = 3;
+// //      d. Show the value of variable in your browser like “Initial value: 3”.
+// var b = "initial value is : "+val1+" " ;
+// alert(b);
+// //      e. Increment the variable.
+// var val2 = ++val1;
+// //      f. Show the value of variable in your browser like “Value after increment is: 4”.
+// var c = "Value after Increament is : "+val2+" ";
+// alert(c);
+// //      g. Add 7 to the variable
+// var val3 = val2 + 7;
+// //      h. Show the value of variable in your browser like “Value after addition is: 11”.
+// var d = "value after addition of 7 is "+val3+" ";
+// alert(d);
+// //      i. Decrement the variable.
+// var val4 = --val3;
+// //      j. Show the value of variable in your browser like “Value after decrement is: 10”.
+// var e = "Value after decreament is "+val4+" ";
+// alert(e);
+// //      k. Show the remainder after dividing the variable’s value by 3. Output : “The remainder is : 1”
+// var val5 = val4 % 3;
+// var f = "Remainder of 10 is "+val5+" ";
+// alert(f);
+
+
+//                      4. Cost of one movie ticket is 600 PKR.
+
+
+// var ticket = 5;
+// var price = 3000;
+// document.write("Total cost to buy "+ticket+" tickets to a Movie is "+price+" PKR "); 
+
+
+//      5. Write a script to display multiplication table of any number in your browser.
+
+// var x = 5;
+// var y ;
+// for(var i = 1;i <= 10 ;i++){
+//     y = x*i
+//     document.write("<h4> "+x+" x "+i+" = "+y+"</h4> ");
+// }
+
+
+//   6. Write a program to implement checkout process of a shopping cart system for an e-commerce website.
+
+// var x = 100;
+// var y = 200;
+// var q1 = 2;
+// var q2 = 3;
+// var ship = 150;
+// document.write("<h4>Price of Item 1 is "+x+" </h4>" );
+// document.write("<h4>Quantity of of Item 1 is "+q1+" </h4>" );
+// document.write("<h4>Price of Item 2 is "+y+" </h4>" );
+// document.write("<h4>Quantity of of Item 2 is "+q2+" </h4>" );
+// document.write("<h4>Shiping charges are "+ship+" </h4>" );
+// document.write("<h4>Total charge of your order is 2270 PKR</h4>" );
+
+
+//                      8. Write a script to take total marks & marks obtained by a student.
+
+// var total = 500;
+// var obt;
+// var eng = 65;
+// var urdu = 76;
+// var chem = 87;
+// var phy = 67;
+// var isl = 79;
+// obt = eng + urdu + chem + phy + isl;
+// per = obt/5;
+// document.write("<h1>MARK SHEET</h1>");
+// document.write("<h3>Total Marks = "+total+"</h3>");
+// document.write("<h3>Marks Obtained = "+obt+"</h3>");
+// document.write("<h3>Total Marks = "+per+"</h3>");
+
+
+// 10. Write a program to initialize a variable with some number
+
+// var no = 3;
+// var x = no + 5 * 10 /2;
+// alert(x);
+
+
+// 11. The Age Calculator: Forgot how old someone is? Calculate it!
+
+var x = 1998;
+var y = 2017;
+var z = 2017 - 1998;
+var a = z - 1 ;
+alert("They are either "+z+" or "+a+" ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
